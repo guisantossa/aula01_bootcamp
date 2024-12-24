10449aa66d7ea940c0f1c73d6729f6dcec37f51f
Guilherme <gui.santos.sa@gmail.com>
2024-12-24 09:28:46 -0300

    desafio

---
+++ desafio.py
@@ -0,0 +1,12 @@
+nome = input("Digite seu nome: ")
+
+salario = float(input("Digite seu Salario: "))
+
+bonus = float(input("Digitte seu Bonus: "))
+
+kpi = 1000 + salario * bonus
+
+print(f"O calculo de KPI é 1000 + {salario} * {bonus}")
+
+print(f"{nome} Seu salário é de {salario} e seu Bonus é {kpi}") 
+
